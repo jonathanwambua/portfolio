d93edfcb166b14b7ac2007891e7e3e6287372b87
wambua-pesapal <jonathan.wambua@pesapal.com>
2023-06-21 19:31:17 +0300

    Updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,15 +19,11 @@ export default function Projects() {
           {projects.map((project) => (
             <a
               href={project.link}
+              target="_blank"
               key={project.image}
               className="sm:w-1/2 w-100 p-4">
               <div className="flex relative">
-                <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src={project.image}
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
@@ -39,6 +35,13 @@ export default function Projects() {
               </div>
             </a>
           ))}
+        </div>
+        <div className="mt-10">
+            <a
+              href="/projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              View More Projects
+            </a>
         </div>
       </div>
     </section>
--- src/data.js
+++ src/data.js
@@ -1,35 +1,27 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    title: "Chuka University CU WebApp",
+    subtitle: "Wordpress | Php | Bootstrap",
+    description: "Build a bible study registration application using wordpress for the website, and php for the backend functionality of the registration process. The system is in use by over 1,500 users.",
+    link: "https://chukaunicu.org",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    title: "Hotel/Restaurant Suggestion",
+    subtitle: "Django, Python",
+    description: "The project allows one to get an analysis of reviews from various hotels/restaurants and with that make a decision of where to spend their next date or even vacation. This happened to be my final year project in my Bachellors Degree study.",
+    link: "https://wambua.dev/projects/reviewer",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+    title: "Laravel POS",
+    subtitle: "Laravel | Vue Js",
+    description: "Built an on premise POS for a client, this involved a complete solution for inventory management and sales, including a funcitonality to print reciepts after a sale. On this project, its exciting that the system has been in use for 2 years now.",
+    link: "https://wambua.dev/project/laravelpos",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+    title: "Open MRS Core",
+    subtitle: "Java",
+    description: "Contributed in a hackathon where the challenge was to improve the customer journey of the current Open MRS core system used by most of the county hospitals in Kenya.",
+    link: "https://github.com/jonathanwambua/Group3Mrs",
   },
 ];
 
@@ -51,10 +43,12 @@ export const testimonials = [
 ];
 
 export const skills = [
+  "Laravel",
+  "PHP",
+  "REST",
   "JavaScript",
-  "React",
-  "Next.js",
+  "Vue",
+  "Nuxt.js",
   "Node",
-  "GraphQL",
   "Material UI",
 ];
